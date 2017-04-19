bc5c77fd2562c09be4bc22ac87e1588eeec90b15
Nikola Divic <divicnikola@gmail.com>
2017-04-19 15:03:47 +0200

    GameCredits python module initial commit
    I extracted some code that we wrote and used
    over and over again when building application that work with
    GameCredits and I encapsulated it inside a pip module.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gamecredits',
+    version='0.1',
+    description='Tools for working with the GameCredits cryptocurrency',
+    url='https://github.com/gamecredits-project/python-gamecredits',
+    author='Nikola Divic',
+    author_email='divicnikola@gmail.com',
+    license='MIT',
+    packages=['gamecredits'],
+    zip_safe=False
+)
