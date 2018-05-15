69b9bb26694f66e60f3f68b9031c96017e2be9e8
Filip Micic <filip.micic@gamecredits.com>
2018-05-15 16:38:00 +0200

    Setup version set to 0.1.4

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='gamecredits',
-    version='0.1.3',
+    version='0.1.4',
     description='Tools for working with the GameCredits cryptocurrency',
     url='https://github.com/gamecredits-project/python-gamecredits',
-    download_url='https://github.com/gamecredits-project/python-gamecredits/archive/0.1.3.tar.gz',
+    download_url='https://github.com/gamecredits-project/python-gamecredits/archive/0.1.4.tar.gz',
     author='Nikola Divic',
     author_email='divicnikola@gmail.com',
     license='MIT',
